5fd571fec7051902c9156ce134a3d0a6d8ca11ec
Samantaks <saksamantha@gmail.com>
2024-06-23 18:15:29 -0300

    novo tópico da aula

--- app/views.py
+++ app/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
+#from django.http import HttpResponse
+#from datetime import datetime
+
 
 
 def index(request):
-    context={
-        "title": "Home Page",
-        "a_date": datetime.now(),
-        "course_title": "Aulas de Django",
-        "user":{
-            "name":"Licia",
-            "email":"cancerina_virginiana@email.com"
-        },
-        "product_price": 146842600.2446,
-        "random_text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore"
-    }
-    
+    context = {
+
+        
+    } 
+
     return render(request, 'index.html', context)
+
+
+#def index(request):
+#    context={
+#        "title": "Home Page",
+#        "a_date": datetime.now(),
+#        "course_title": "Aulas de Django",
+#        "user":{
+#            "name":"A",
+#            "email":"cancerina_virginiana@email.com"
+#        },
+#        "product_price": 146842600.2446,
+#        "random_text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore"
+#    }
+#    return render(request, 'index.html', context)
